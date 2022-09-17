dc9a2ff8f359568b78d1426f4236037547a51c3a
Onurakdgn <86933632+Onurakdgn@users.noreply.github.com>
2022-09-18 00:20:34 +0300

    code

---
+++ javaOdevIntro/src/datatypes/Main.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi = 12.8;
+		sayi = -125;
+
+		char karakter = 'b';
+		boolean dogruMu = true;
+
+		System.out.println(sayi);
+		System.out.println(karakter);
+		System.out.println(dogruMu);
+
+	}
+
+}
---
+++ javaOdevIntro/src/veriables/Main.java
@@ -0,0 +1,17 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci Sayisi : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci Sayisi : " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayisi : " + ogrenciSayisi);
+		System.out.println("Öğrenci Sayisi : " + ogrenciSayisi);
+
+	}
+
+}
